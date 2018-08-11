ee81404771ea664b41c89a31f56a60866d666c32
Rage <38593512+Rage0001@users.noreply.github.com>
2018-08-11 17:27:52 +0300

    Added customisable approval/denial emotes

--- commands/submit.js
+++ commands/submit.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 const utils = require("../utils");
-const config = require("../config");
 const db = require("quick.db");
 
 module.exports.run = async (client, message, args) => {
@@ -16,7 +15,7 @@ module.exports.run = async (client, message, args) => {
   if(!args[1].startsWith("http")){
     return message.channel.send("Provide a valid link of the emote.")
   } else{
-    let reportID = await db.fetch('reportID')
+    reportID = await db.fetch('reportID')
        if(reportID != null){
          await db.add('reportID', 1)
        }
@@ -27,7 +26,7 @@ module.exports.run = async (client, message, args) => {
     message.react("👀")
     message.channel.send("Your emote has been sent to the Ghoulers for approval!").then(m => m.delete(5000))
     db.fetch("reportID").then(number => {
-      client.channels.get(utils.logChannel).send(`<:approve:475044157904125962> \`${args[0]}\` Submitted by ${message.author}. Submission ID: (\`#${number}\`)`)
+      client.channels.get(utils.logChannel).send(`<:approve:${utils.approveEmote}> \`${args[0]}\` Submitted by ${message.author}. Submission ID: (\`#${number}\`)`)
       client.channels.get(utils.queueChannel).send(`-------------------------------\n**The following ghoul-mote requires approval:**\n**Submitted by:** ${message.author}\n**Name:** \`${args[0]}\`\n**Link:** ${args[1]}\n**Submission ID:** #${number}\n-------------------------------`).then(msg => {
       msg.react(utils.denyEmote)
       msg.react(utils.approveEmote)
